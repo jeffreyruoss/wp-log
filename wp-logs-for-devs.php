f992cc504f56c0c4a0572c00d3c71e5fb8ce8712
Jeff Ruoss <jeff@zenthree.com>
2020-06-17 17:00:41 -0700

    Create main plugin file and header.

---
+++ wp-logs-for-devs.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WP Logs for Devs
+ * Plugin URI: https://jeffruoss.com
+ * Description: A log system for developers.
+ * Author: Jeff Ruoss
+ * Version: 1
+ * Author URI: https://jeffruoss.com
+ * Text Domain: wp-logs-for-devs
+ */
+
+
